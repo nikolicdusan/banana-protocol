dc0f9eb1aaaceb87a70071a4ba909c3ec7074c0a
Dušan Nikolić <thenikolicdusan@gmail.com>
2025-12-11 21:19:04 +0100

    feature: project setup

---
+++ BananaProtocol.API/Common/Http/ApiControllerBase.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace BananaProtocol.API.Common.Http;
+
+[ApiController]
+[Route("api/[controller]")]
+public class ApiControllerBase : ControllerBase
+{
+}
---
+++ BananaProtocol.Infrastructure/ConfigureServices.cs
@@ -0,0 +1,11 @@
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BananaProtocol.Infrastructure;
+
+public static class ConfigureServices
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        return services;
+    }
+}
